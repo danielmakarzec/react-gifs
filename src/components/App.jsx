4f1edba77b1d2342f923ee4d16aa3f88cdd6cfc8
daniel makarzec <danielmakarzec@gmail.com>
2020-01-19 17:06:49 +0000

    create App component

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Search from './Search';
+
+const App = () => {
+  return (
+    <div>
+      <h1>
+        <Search />
+      </h1>
+    </div>
+  );
+};
+
+export default App;
